78130634267deec713c3ad14352c5b29937a2044
Greateki <ekeneidiagbor@gmail.com>
2021-09-23 15:39:51 +0100

    chore: connected to mongodb

--- server/server.js
+++ server/server.js
@@ -4,10 +4,14 @@ const dotenv = require('dotenv');
 const path = require('path');
 const app = require('./app');
 
+const DbConnection = require('./config/database');
+
 // Create server
 const server = http.createServer(app);
 
 dotenv.config({ path: path.resolve(__dirname, './config/config.env') });
+
+DbConnection();
 
 const PORT = process.env.PORT || 5000;
 
